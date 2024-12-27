7fa13dc46c0705b596c7e9021726c03a1e79bcf9
Poshak <poshakkumarbanger@gmail.com>
2024-12-27 10:54:37 +0530

    commit changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,6 +16,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Verify from './pages/Verify';
 
+// this is App.jsx file
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
