4a02083194613be7fde4bcaa38930ca484340032
Poshak <poshakkumarbanger@gmail.com>
2024-12-31 11:20:50 +0530

    changes in admin vite.config.js file

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,13 +1,32 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {port: 5174},
+//   optimizeDeps: {
+//     include: ['react-router-dom']
+//   },
+//   build: {
+//     rollupOptions: {
+//       external: ['react-router-dom']
+//     }
+//   }
+// });
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {port: 5174},
+  optimizeDeps: {
+    include: ['react-router-dom']
+  },
   build: {
-    rollupOptions: {
-      external: ['react-router-dom']
+    commonjsOptions: {
+      include: [/react-router-dom/, /node_modules/]
     }
   }
-});
+})
