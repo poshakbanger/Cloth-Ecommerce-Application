2ed127d5929bf5e402db3edd459451bfb4664229
Poshak <poshakkumarbanger@gmail.com>
2025-01-05 20:07:48 +0530

    changes done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,23 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server : {port:5173},
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server : {port:5173},
-  eslint: {
-    ignoreDuringBuilds: true,
-  }
+  server: { port: 5173 },
+  build: {
+    outDir: 'dist', // Ensure output directory is correct
+  },
 })
