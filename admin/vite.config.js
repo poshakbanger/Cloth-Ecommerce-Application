78c2984409964493773a08f630f50c5c8333551a
Poshak <poshakkumarbanger@gmail.com>
2025-01-11 23:43:45 +0530

    changes

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,19 +1,19 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {port: 5174},
-//   optimizeDeps: {
-//     include: ['react-router-dom']
-//   },
-//   build: {
-//     rollupOptions: {
-//       external: ['react-router-dom']
-//     }
-//   }
-// });
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {port: 5174},
+  optimizeDeps: {
+    include: ['react-router-dom']
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom']
+    }
+  }
+});
 
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
@@ -31,18 +31,18 @@
 //   }
 // })
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {port: 5174},
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom']
-  },
-  build: {
-    commonjsOptions: {
-      include: [/node_modules/]
-    }
-  }
-});
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {port: 5174},
+//   optimizeDeps: {
+//     include: ['react', 'react-dom', 'react-router-dom']
+//   },
+//   build: {
+//     commonjsOptions: {
+//       include: [/node_modules/]
+//     }
+//   }
+// });
