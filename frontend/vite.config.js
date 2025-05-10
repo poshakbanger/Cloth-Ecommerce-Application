e782d315362e9980db3dd65ecf282f6780671b2f
Poshak <poshakkumarbanger@gmail.com>
2025-05-10 19:38:24 +0530

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server : {port:5173},
-  build: {rollupOptions:{
-    external:[]
-  }}
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
+  // For client-side routing
+  server: {
+    historyApiFallback: true
+  }
 })
