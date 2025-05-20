b5fbca58aee22af83e919fde8ebd7aaaeb7e7a2c
Poshak <poshakkumarbanger@gmail.com>
2025-05-20 15:28:50 +0530

    commit

---
+++ admin/vite.config.js
@@ -0,0 +1,43 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {port : 5174},
+  build: {rollupOptions:{
+    external:[]
+  }}
+});
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {port: 5174},
+//   optimizeDeps: {
+//     include: ['react-router-dom']
+//   },
+//   build: {
+//     commonjsOptions: {
+//       include: [/react-router-dom/, /node_modules/]
+//     }
+//   }
+// })
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {port: 5174},
+//   optimizeDeps: {
+//     include: ['react', 'react-dom', 'react-router-dom']
+//   },
+//   build: {
+//     commonjsOptions: {
+//       include: [/node_modules/]
+//     }
+//   }
+// });
